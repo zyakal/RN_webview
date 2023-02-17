a0c34d132a6f3177b62e735a529f17bafca7d0bf
zyakal <pkd1426@gmail.com>
2023-02-17 13:22:36 +0900

    modified:   App.js

--- App.js
+++ App.js
@@ -3,11 +3,11 @@ import { WebView } from "react-native-webview";
 
 export default function App() {
   return Platform.OS === "web" ? (
-    <iframe src="http://192.168.0.115:3000/" height={'100%'} width={'100%'} />
+    <iframe src="https://jcidg.com/" height={'100%'} width={'100%'} />
   ) : (
     <View style={{ flex: 1 }}>
       <WebView
-        source={{ uri: "http://192.168.0.115:3000/" }}
+        source={{ uri: "https://jcidg.com/" }}
         style={{ marginTop: 22, flex: 1 }}
       />
     </View>
